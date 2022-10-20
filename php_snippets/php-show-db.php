14e09940d1382438422dd218388053670c164f54
neil_liang <neil_liang@arcadyan.com.tw>
2022-10-20 18:21:20 +0800

    [bwtest] Update code layout.
    Description:
    20221020

--- php_snippets/php-show-db.php
+++ php_snippets/php-show-db.php
@@ -18,55 +18,99 @@
 ?>
 
 <?php
-	$query = "SELECT * FROM bwtest ORDER BY timestamp"; //搜尋 *(全部欄位) ，從 表staff
+	$query = "SELECT * FROM bwtest ORDER BY timestamp DESC"; //搜尋 *(全部欄位) ，從 表staff
 
 	//mysqli_query << PHP 有很多種...指令(?) ，這是其中一個，我現在還都是學到甚麼用什麼，沒辦法自己看手冊，然後實驗+學習使用。 
 
 	$query_run = mysqli_query($con,$query); //$con <<此變數來自引入的 db_cn.php
 ?>
-
-<div class="container">
-	<table class="table table-sm table-bordered"style="text-align:center;">
-		<thead style="text-align:center;">
-			<tr style="text-align:center;">
-				<th>time</th>
-				<th>author</th>
-				<th>title</th>
-				<th>short_description</th>
-				<th>content</th>
-				<th>pic_url</th>
-				<th>timestamp</th>
-			</tr>
-		</thead>
-
-		<tbody>
 			<?php
 				if(mysqli_num_rows($query_run) > 0)
 				{
+					$column_idx=0;
 					foreach($query_run as $row)
 					{
+						if($column_idx == 0)
+						{
 			?>
-							<tr>
-								<td><?php echo $row["time"]; ?></td> 
-								<td><?php echo $row["author"]; ?></td> 
-								<td><?php echo $row["title"]; ?></td>
-								<td><?php echo $row["short_description"]; ?></td> 
-								<td><?php echo $row["content"]; ?></td> 
-								<td><?php echo $row["pic_url"]; ?></td>
-								<td><?php echo $row["timestamp"]; ?></td>
-							</tr>
-							<!-- wp:image {"align":"full","sizeSlug":"large","className":"is-style-default"} -->
-<figure class="wp-block-image alignfull size-large is-style-default"><img src="https://drive.google.com/uc?export=view&amp;id=<?php echo $row["pic_url"]; ?>" alt="<?php echo $row["short_description"]; ?>"/>
-<br><a href="http://testsite2.test/2022/10/18/new-page/?title=<?php echo $row['title']; ?>"><?php echo $row["title"]; ?></a>
-<figcaption><?php echo $row["short_description"]; ?></figcaption></figure><br><br><br>
-							<!-- /wp:image -->
+<!-- wp:group {"align":"wide"} -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"verticalAlignment":"top","align":"full","style":{"color":{"text":"#000000","background":"#ffffff"}}} -->
+<div class="wp-block-columns alignfull are-vertically-aligned-top has-text-color has-background" style="background-color:#ffffff;color:#000000">
 			<?php
+						}
+			?>
+<a href="http://testsite2.test/2022/10/18/new-page/?title=<?php echo $row['title']; ?>">
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:image {"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://drive.google.com/uc?export=view&amp;id=<?php echo $row["pic_url"]; ?>" alt=""/><figcaption><gwmw style="display:none;"><gwmw style="display:none;"></gwmw><gwmw style="display:none;"></figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+<h3 style="font-size:24px;line-height:1.3"><strong><?php echo $row["title"]; ?></strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo $row["time"]; ?></p>
+<p style="width: 240px; word-break: break-all;word-wrap: break-word;"><?php echo $row["short_description"]; ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+</a>
+			<?php
+						if($column_idx == 3) //結尾
+						{
+			?>
+</div>
+			<?php
+						}
+						else //column 間空白
+						{
+			?>
+&emsp;
+&emsp;
+			<?php
+						}
+
+						if($column_idx<3) $column_idx++;
+						else $column_idx=0;
 					}
+					if($column_idx != 0) //如果4個column排不滿，要多塞隱形的column來維持版面
+					{
+						for($idx=$column_idx; $idx<4; $idx++) //因為離開前一個loop時有++，所以前一個loop column_idx=0的在這邊代表column_idx=1，就是只印了一個column，那代表右邊要多塞3個隱形的column
+						{
+			?>
+<div style="visibility:hidden">
+<a href="http://testsite2.test/2022/10/18/new-page/?title=<?php echo $row['title']; ?>">
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:image {"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://drive.google.com/uc?export=view&amp;id=<?php echo $row["pic_url"]; ?>" alt=""/><figcaption><gwmw style="display:none;"><gwmw style="display:none;"><gwmw style="display:none;"></gwmw></gwmw></gwmw></figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+<h3 style="font-size:24px;line-height:1.3"><strong><?php echo $row["title"]; ?></strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo $row["time"]; ?></p>
+<p style="width: 240px; word-break: break-all;word-wrap: break-word;"><?php echo $row["short_description"]; ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+</a>
+</div>
+			<?php
+							if($idx<3) //column 間空白
+							{
+			?>
+&emsp;
+&emsp;
+			<?php
+							}
+						}
+					}
+			?>
+</div>
+<!-- /wp:columns -->
+<!-- /wp:group -->
+			<?php
 				}
 			?>
-		</tbody>
-	</body>
-
-</div>
-
 <?php mysqli_close($con);?>
